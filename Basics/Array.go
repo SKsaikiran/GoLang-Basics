e255a4613dcd157d1877a31d95d24c3fd6c0859d
Sai kiran J <saikiranj2002@gmail.com>
2022-11-22 19:18:29 +0530

    Create Array.go

---
+++ Basics/Array.go
@@ -0,0 +1,13 @@
+// Program to create an array and prints its elements
+
+package main
+import "fmt"
+
+func main() {
+   
+  // declare array variable of type integer
+  // defined size [size=3]
+  var arrayOfInteger = [3]int{2,4,6}
+  
+  fmt.Println(arrayOfInteger)
+}
