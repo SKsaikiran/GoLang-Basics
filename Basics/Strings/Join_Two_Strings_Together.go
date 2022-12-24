962a09f68eb98b1daab054845310cc3ac343483c
Sai kiran J <saikiranj2002@gmail.com>
2022-12-24 22:04:32 +0530

    Create Join_Two_Strings_Together.go

---
+++ Basics/Strings/Join_Two_Strings_Together.go
@@ -0,0 +1,14 @@
+// Program to concatenate two strings
+
+package main
+import "fmt"
+
+func main() {  
+  message1 := "I wanna learn"
+  message2 := "Go programming"
+    
+  // concatenation using + operator
+  result := message1 + " " + message2
+
+  fmt.Println(result)
+}
