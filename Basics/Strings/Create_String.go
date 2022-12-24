6edf5c276a721c2f1ab2fc531f77359ac9d3ec39
Sai kiran J <saikiranj2002@gmail.com>
2022-12-24 21:30:14 +0530

    Add files via upload

---
+++ Basics/Strings/Create_String.go
@@ -0,0 +1,16 @@
+// Program to create a string in Golang
+
+package main
+import "fmt"
+
+func main() {
+
+  // create string using var
+  var message1 = "Hello"
+
+  // create string using shorthand notation
+  message2 := "Welcome to My Repo"
+
+  fmt.Println(message1)
+  fmt.Println(message2)
+}
