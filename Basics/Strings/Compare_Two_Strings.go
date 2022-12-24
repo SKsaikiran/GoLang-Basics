e674aca82803cfcc407e85ed437189bbfb02fbfc
Sai kiran J <saikiranj2002@gmail.com>
2022-12-24 22:12:29 +0530

    Create Compare_Two_Strings.go

---
+++ Basics/Strings/Compare_Two_Strings.go
@@ -0,0 +1,21 @@
+// Program to compare string using Compare()
+
+package main
+import (
+  "fmt"
+  "strings"
+)
+
+func main() {
+
+  // create three strings
+  string1 := "GOlang"
+  string2 := "GOlang Pro"
+  string3 := "GO"
+
+  // compare strings
+  fmt.Println(strings.Compare(string1, string2))  // -1
+  fmt.Println(strings.Compare(string2, string3))  // 1
+  fmt.Println(strings.Compare(string1, string3))  // 0
+
+}
