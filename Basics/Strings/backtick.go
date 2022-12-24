f86772b32a7565b7e52fe08e69280fdc307058cf
Sai kiran J <saikiranj2002@gmail.com>
2022-12-24 21:59:32 +0530

    Create backtick.go

---
+++ Basics/Strings/backtick.go
@@ -0,0 +1,12 @@
+Program to represent a string with a backtick
+
+package main
+import "fmt"
+
+func main() {
+
+  //  represent string with `  `    
+  message := `I am learning Go Programming`
+    
+  fmt.Println(message)
+}
