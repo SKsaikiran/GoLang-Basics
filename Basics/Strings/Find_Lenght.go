d2fd6183d0c3c92c6844d4fd3453b0184446a7c2
Sai kiran J <saikiranj2002@gmail.com>
2022-12-24 22:03:05 +0530

    Create Find_Lenght.go

---
+++ Basics/Strings/Find_Lenght.go
@@ -0,0 +1,16 @@
+// Program to count the length of a string
+
+package main
+import "fmt"
+ 
+func main() {
+ 
+  // create string
+  message := "Welcome to My Repo"
+    
+  // use len() function to count length
+  stringLength := len(message)
+
+  fmt.Println("Length of a string is:", len(stringLength))
+ 
+}
